5459b5bc2130536b8e455abc1db4b75e08b38daf
inpos12 <inpos_@naver.com>
2024-09-09 11:11:47 +0900

    LastVersion

--- Web/Screen/SubPage_5/SubPage_5_6/SubPage5_6.js
+++ Web/Screen/SubPage_5/SubPage_5_6/SubPage5_6.js
@@ -4,6 +4,9 @@ $.get("/system/Web/Common/Navbar/Navbar.html", function (data) {
 $.get("/system/Web/Common/Footer/Footer.html", function (data) {
   $("footer").html(data);
 });
+$.get("/system/Web/Common/Comming/Comming.html", function (data) {
+  $("Loading").html(data);
+});
 $.get(
   "/system/Web/Screen/SubPage_5/SubPage_5_6/Section_1/Section_1.html",
   function (data) {
